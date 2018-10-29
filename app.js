29dd4fb74f02b40fab505a3abf99f866e681e3d8
Akshay Tiwari <37179627+tvibhu12@users.noreply.github.com>
2018-10-29 23:23:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 
+$(function(){
+    var tabs = $('#features > nav a');
+    var tabsContent=$('#features > article > section');
+   
+   
+    tabs.click(function(e){
+    e.preventDefault();
+    var that=$(this);
+    tabs.removeClass('is-selected');
+    that.addClass('is-selected');
+    tabsContent.removeClass('is-selected');
+
+    tabsContent.filter((i,tab)=>$(tab).data('id')===that.data('id')).addClass('is-selectd');
+
+   });
